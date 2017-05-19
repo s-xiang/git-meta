ab5c55b596414111b77a0023a5beb8094b3e34e6
Brock Peabody <brock.peabody@gmail.com>
2017-05-19 15:37:02 -0400

    path for 872ec0c8364b4775d209b5e56226295137142fea (the `listworkdir tree` commit)

--- node/lib/util/commit.js
+++ node/lib/util/commit.js
@@ -38,7 +38,6 @@ const assert  = require("chai").assert;
 const co      = require("co");
 const colors  = require("colors");
 const NodeGit = require("nodegit");
-const path    = require("path");
 
 const DiffUtil            = require("./diff_util");
 const GitUtil             = require("./git_util");
@@ -538,7 +537,6 @@ exports.writeRepoPaths = co.wrap(function *(repo, status, message) {
     assert.instanceOf(status, RepoStatus);
     assert.isString(message);
 
-    const workdir = repo.workdir();
     const headCommit = yield repo.getHeadCommit();
     const changes = {};
     const staged = status.staged;
@@ -559,8 +557,7 @@ exports.writeRepoPaths = co.wrap(function *(repo, status, message) {
             changes[filename] = null;
         }
         else {
-            const filePath = path.join(workdir, filename);
-            const blobId = GitUtil.hashFile(repo, filePath);
+            const blobId = TreeUtil.hashFile(repo, filename);
             changes[filename] = new Change(blobId, FILEMODE.BLOB);
 
             yield index.addByPath(filename);
